acd04295039aa9a6ee4099a105328aa0df785151
Kevin Neaton <kevin@neaton.xyz>
2022-10-19 23:52:08 -0400

    add umami tracking code to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const Home: NextPage = () => {
         <title>Welcome to Big Blue Meenie</title>
         <meta name="description" content="Big Blue Meenie is one of the oldest, largest, and certainly the most prolific Open for Hire private production houses for rock music on the east coast of America." />
         <link rel="icon" href="/favicon.ico" />
+        <script async defer data-website-id="e5593e8e-43b6-4351-8cad-9c4b13fb2ed8" src="https://umami.release.club/umami.js"></script>
       </Head>
 
       <main className="grid grid-cols-8 gap-px p-1 pt-24 max-w-max mx-auto">
